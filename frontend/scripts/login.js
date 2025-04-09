bb1d0b5768d8a2654b9429cdab7af3a9a3388922
Checco <f.laurendi.2696@vallauri.edu>
2025-04-09 19:18:07 +0200

    modificato login

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
       request.fail(errore);
       request.done(function(data) {
 
-        console.log(`Logged in:`, data);
+        console.log(`Logged in:`, data.data);
 
       });
 
