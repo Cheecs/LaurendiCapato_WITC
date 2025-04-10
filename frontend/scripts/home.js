099e39f8c9fe84693e0669ec5340b0b9bb31dd06
Checco <f.laurendi.2696@vallauri.edu>
2025-04-10 20:55:46 +0200

    implementazione responsive

--- frontend/scripts/home.js
+++ frontend/scripts/home.js
@@ -23,8 +23,30 @@ function UIsetup(){
     $("#seeMoreBtn").click(function(){
         handleSeeMoreAnimation();
     });
+
+    $(window).on('resize', function() {
+
+        checkWindow()
+    });
+
+    checkWindow();
     
     $('#year').text(new Date().getFullYear());
+}
+
+function checkWindow(){
+
+    if ($(window).width() <= 1004) {
+
+        $("#infoCards").removeClass("col-4");
+        $("#infoCards").addClass("col-10");
+
+    } else {
+
+        $("#infoCards").removeClass("col-10");
+        $("#infoCards").addClass("col-4");
+
+    }
 }
 
 
@@ -33,64 +55,118 @@ function handleSeeMoreAnimation(){
     const containerShortInfo = $("#containerShortInfo");
     const infoCards = $("#infoCards");
     const divInfo = $("#divInfo");
-    const divRev = $("#divRev")
+    const divRev = $("#divRev");
 
-    if(seeMore == false){
+    if ($(window).width() <= 1004) {
+
+        if(seeMore == false){
         
-        divRev.removeClass("slideUp");
-        divRev.addClass("slideDown");
-
-        setTimeout(function(){    
-
-            containerShortInfo.removeClass("slideCenter");
-            containerShortInfo.addClass("slideLeft");
-            containerShortInfo.removeClass("alignCenterShortInfo");
-            containerShortInfo.addClass("alignShortInfo");
+            setTimeout(function(){    
     
-            divInfo.addClass("alignCards");
+                containerShortInfo.removeClass("slideCenter");
+                containerShortInfo.addClass("slideLeft");
+
+        
+                divInfo.addClass("alignCards");
+        
+                infoCards.removeClass("fadeOut");
+                infoCards.addClass("fadeIn");
+                infoCards.removeClass("d-none");
     
-            infoCards.removeClass("fadeOut");
-            infoCards.addClass("fadeIn");
-            infoCards.removeClass("d-none");
+                
+                $("#seeMoreBtn").text("See less");
+    
+            }, 140);
+    
+            seeMore = true;
+        }
+        else{
 
-            
-            $("#seeMoreBtn").text("See less");
+            infoCards.addClass("fadeOut");
+            infoCards.removeClass("fadeIn");  
 
-        }, 140);
+            setTimeout(function(){
+    
+                infoCards.addClass("d-none");
 
-        seeMore = true;
+                containerShortInfo.removeClass("slideLeft");
+                containerShortInfo.addClass("slideCenter");
+        
+                containerShortInfo.removeClass("slideCenter");
+        
+                divInfo.removeClass("alignCards");
+    
+                $("#seeMoreBtn").text("See more");
+                
+            }, 320);  
+    
+            seeMore = false;
+        }
+    
+
     }
     else{
 
-        divRev.addClass("slideUp");
-        divRev.removeClass("slideDown");
-
-        setTimeout(function(){
-
-            containerShortInfo.removeClass("slideLeft");
-            containerShortInfo.addClass("slideCenter");
+        if(seeMore == false){
+        
+            divRev.removeClass("slideUp");
+            divRev.addClass("slideDown");
     
-            infoCards.addClass("fadeOut");
-            infoCards.removeClass("fadeIn");
-
-        }, 140);
-
-        setTimeout(function(){
-
-            infoCards.addClass("d-none");
-
-            containerShortInfo.removeClass("alignShortInfo");
-            containerShortInfo.addClass("alignCenterShortInfo");
-
-            containerShortInfo.removeClass("slideCenter");
+            setTimeout(function(){    
     
-            divInfo.removeClass("alignCards");
+                containerShortInfo.removeClass("slideCenter");
+                containerShortInfo.addClass("slideLeft");
+                containerShortInfo.removeClass("alignCenterShortInfo");
+                containerShortInfo.addClass("alignShortInfo");
+        
+                divInfo.addClass("alignCards");
+        
+                infoCards.removeClass("fadeOut");
+                infoCards.addClass("fadeIn");
+                infoCards.removeClass("d-none");
+    
+                
+                $("#seeMoreBtn").text("See less");
+    
+            }, 140);
+    
+            seeMore = true;
+        }
+        else{
+    
+            divRev.addClass("slideUp");
+            divRev.removeClass("slideDown");
+    
+            setTimeout(function(){
+    
+                containerShortInfo.removeClass("slideLeft");
+                containerShortInfo.addClass("slideCenter");
+        
+                infoCards.addClass("fadeOut");
+                infoCards.removeClass("fadeIn");
+    
+            }, 140);
+    
+            setTimeout(function(){
+    
+                infoCards.addClass("d-none");
+    
+                containerShortInfo.removeClass("alignShortInfo");
+                containerShortInfo.addClass("alignCenterShortInfo");
+    
+                containerShortInfo.removeClass("slideCenter");
+        
+                divInfo.removeClass("alignCards");
+    
+                $("#seeMoreBtn").text("See more");
+                
+            }, 600);  
+    
+            seeMore = false;
+        }
+    
 
-            $("#seeMoreBtn").text("See more");
-            
-        }, 600);  
-
-        seeMore = false;
     }
+
 
 }
