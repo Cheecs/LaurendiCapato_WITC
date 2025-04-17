819bde7587971876363a24d3c1f2517c00b22874
Checco <f.laurendi.2696@vallauri.edu>
2025-04-17 14:15:23 +0200

    modificato product completata implementazione analisi immagine

--- frontend/scripts/product.js
+++ frontend/scripts/product.js
@@ -15,7 +15,53 @@ $(document).ready(function(){
 
 function writeInTable(data){
 
-    console.log(data);
+    let tBodyColor = $("#tBodyColor");
+    let tBodyPalette = $("#tBodyPalette");
+
+    /* Main color */
+
+    let tr = $("<tr>");
+
+    let tdColore = $("<td class='tdColor'>");
+
+    let divColore = $("<div class='divColor'></div>");
+    divColore.css("backgroundColor", data.colorHEX);
+    tdColore.append(divColore);
+
+    let tdRGB = $(`<td class="tdText">${data.colorRGB}</td>`); 
+    let tdHEX = $(`<td class="tdText">${data.colorHEX}</td>`);
+
+    tr.append(tdColore);
+    tr.append(tdHEX);
+    tr.append(tdRGB);
+
+    tBodyColor.append(tr);
+
+    /* Palette */
+
+    let paletteHEX = data.paletteHEX;
+    let paletteRGB = data.paletteRGB;
+    
+    for(let i = 0; i < paletteHEX.length; i++){
+
+        let tr = $("<tr>");
+
+        let tdColore = $("<td class='tdColor'>");
+
+        let divColore = $("<div class='divColor'></div>");
+        divColore.css("backgroundColor", paletteHEX[i]);
+        tdColore.append(divColore);
+
+        let tdRGB = $(`<td class="divText">${paletteRGB[i]}</td>`);
+        let tdHEX = $(`<td class="tdText">${paletteHEX[i]}</td>`);
+
+        tr.append(tdColore);
+        tr.append(tdHEX);
+        tr.append(tdRGB);
+
+        tBodyPalette.append(tr);
+
+    }
 
 }
 
