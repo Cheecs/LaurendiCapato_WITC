280bb45421d8b5b1e9aca84a8a2f2fe842dab7d4
Checco <f.laurendi.2696@vallauri.edu>
2025-05-19 12:24:35 +0200

    modificata signup

--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -1,38 +1,66 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $("#showPwd").prop("checked", false);
+  $("#showPwd").prop("checked", false);
+  handleShowHidePwd();
+  redirect();
 
-    handleShowHidePwd();
+  async function redirect() {
 
-    $("#signupForm").submit(function(event) {
+    let token = sessionStorage.getItem("token");
 
-        event.preventDefault();
-    
-        let username = $("#username").val();
-        let email = $("#email").val();
-        let pwd = $("#password").val();
-        let hasPwd = CryptoJS.MD5(pwd).toString();
-    
-        let reqBody = {
-          mail: email,
-          usrName: username,
-          pwd: hasPwd
-        };
-        
-        let request = inviaRichiesta("POST", "/api/signup", reqBody);
-        
-        request.fail((err) => {
-  
-          showAlert(err.responseJSON.msg);
-  
-        });
-        request.done(function(data) {
-            
-            // salvare i dati utente
+    if (token) {
+      let validToken = await validateToken(token)
 
-            window.open("./product.html", "_self");
-  
-        });
-  
-      });
+      if (validToken)
+        window.open("./product.html", "_self");
+    }
+
+  }
+
+  $("#signupForm").submit(function (event) {
+
+    event.preventDefault();
+
+    let username = $("#username").val();
+    let email = $("#email").val();
+    let pwd = $("#password").val();
+    let hasPwd = CryptoJS.MD5(pwd).toString();
+
+    let reqBody = {
+      mail: email,
+      usrName: username,
+      pwd: hasPwd
+    };
+
+    let request = inviaRichiesta("POST", "/api/signup", reqBody);
+
+    request.fail((err) => {
+
+      showAlert(err.responseJSON.msg);
+
+    });
+    request.done(function (data) {
+
+      // salvare i dati utente
+
+      window.open("./product.html", "_self");
+
+    });
+
+  });
 });
+
+function validateToken(_token) {
+
+  let reqBody = {
+    token: _token
+  };
+
+  return new Promise((resolve, reject) => {
+    let request = inviaRichiesta("POST", "/api/decodeToken", reqBody);
+
+    request.done((data) => {
+      resolve(data.data, true);
+    });
+  });
+}
