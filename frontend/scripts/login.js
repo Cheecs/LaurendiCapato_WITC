70bbfb6c48581d9533e1b31bfe8a9dc512389342
Checco <f.laurendi.2696@vallauri.edu>
2025-05-19 11:48:58 +0200

    modificato login

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
     $("#showPwd").prop("checked", false);
     handleShowHidePwd();
 
-          let token = sessionStorage.getItem("token");
+      let token = sessionStorage.getItem("token");
 
       if(token)
-        window.location.href = "./product.html";
+        console.log(token);
+        //window.location.href = "./product.html";
   
     $("#signInForm").submit(function(event) {
 
